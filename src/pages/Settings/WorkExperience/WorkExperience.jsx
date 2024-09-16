43b138245893049045a2a7c41b3c08b4fac05a7e
Iskandar <iskndr640@gmail.com>
2024-09-16 10:13:12 +0400

    m

--- src/pages/Settings/WorkExperience/WorkExperience.jsx
+++ src/pages/Settings/WorkExperience/WorkExperience.jsx
@@ -13,37 +13,35 @@ const WorkExperience = () => {
   };
   return (
     <>
-      <main className={style.main}>
-        <div className={style.addExp}>
-          <div>
-            <h2>Work experience</h2>
-            <p>Enhance your visibility by adding work experience.</p>
-          </div>
-          <button
-            className={style.addButton}
-            onClick={() => handleAddExp()}
-            type="button"
-          >
-            <img src={add} alt="" />
-            Add Experience
-          </button>
+      <div className={style.addExp}>
+        <div>
+          <h2>Work experience</h2>
+          <p>Enhance your visibility by adding work experience.</p>
         </div>
-        <TaskForm ref={modal} />
-        <div className={style.experienceContainer}>
-          <div className={style.expText}>
-            <h5>Experience</h5>
-            <p>
-              {`Elevate your profile's appeal by providing detailed work
+        <button
+          className={style.addButton}
+          onClick={() => handleAddExp()}
+          type="button"
+        >
+          <img src={add} alt="" />
+          Add Experience
+        </button>
+      </div>
+      <TaskForm ref={modal} />
+      <div className={style.experienceContainer}>
+        <div className={style.expText}>
+          <h5>Experience</h5>
+          <p>
+            {`Elevate your profile's appeal by providing detailed work
                 experience.`}
-            </p>
-          </div>
-          <div className={style.experienceList}>
-            {store.user.experience.map((exp) => (
-              <ExperienceCard key={exp.id} experience={exp} />
-            ))}
-          </div>
+          </p>
         </div>
-      </main>
+        <div className={style.experienceList}>
+          {store.user.experience.map((exp) => (
+            <ExperienceCard key={exp.id} experience={exp} />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
